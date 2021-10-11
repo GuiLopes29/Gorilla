9b5d237e9dd1b25e56714d7e75ff053d7fcbbdf9
Guilherme <33187657+GuiLopes29@users.noreply.github.com>
2021-10-10 23:37:31 -0300

    fix:Logger

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('./server');
 const config = require('./config');
 const { cdi } = require('./src/utils/file-reader');
+const logger = require('./src/middleware/logger')
+
+app.use(logger.logger);
+app.use(logger.errorLog);
 
 app.listen(config.port, () => {
     cdi();
